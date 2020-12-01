d083ae0a2516b19001ea7c14ff1458fcd7d44559
Zheltishev <69011257+Zheltishev@users.noreply.github.com>
2020-12-01 10:30:16 +0300

    ex14_js-context (#18)
    * ex14_js-context
    
    * fix code style

---
+++ src/ex14_js-context/task-01.js
@@ -0,0 +1,62 @@
+class Calculator {
+  static value = 0;
+
+  constructor() {
+    if (typeof Calculator.instance === 'object') {
+      return Calculator.instance;
+    }
+
+    this.value = 0;
+    Calculator.instance = this;
+
+    return this;
+  }
+
+  static add(el = 0) {
+    this.value += el;
+
+    return this;
+  }
+
+  static subtract(el = 0) {
+    this.value -= el;
+
+    return this;
+  }
+
+  static multiply(el = 1) {
+    this.value *= el;
+
+    return this;
+  }
+
+  static divide(el = 1) {
+    this.value /= el;
+
+    return this;
+  }
+
+  static setState(el) {
+    this.value = el;
+
+    return this;
+  }
+
+  static reset() {
+    this.value = 0;
+
+    return this;
+  }
+
+  static fetchData(callback) {
+    setTimeout(() => callback(500), 1000);
+
+    return this;
+  }
+
+  static getResult() {
+    return this.value;
+  }
+}
+
+module.exports = Calculator;
